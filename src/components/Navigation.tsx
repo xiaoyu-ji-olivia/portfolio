6632b73fb98741fc12f9c08c17df0d4891f0a126
xiaoyu-ji-olivia <84033670+xiaoyu-ji-olivia@users.noreply.github.com>
2026-01-18 14:16:33 -0500

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,8 +17,8 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 
 const drawerWidth = 240;
-const navItems = [['Expertise', 'expertise'], ['History', 'history'], ['Projects', 'projects'], ['Contact', 'contact']];
-
+const navItems = [['Expertise', 'expertise'], ['History', 'history'], ['Projects', 'projects']];
+// , ['Contact', 'contact']
 function Navigation({parentToChild, modeChange}: any) {
 
   const {mode} = parentToChild;
